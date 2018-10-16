fef65e284835de52dd983b30bd21ef7a5dfce34d
mingyang <jsps595214@gmail.com>
2018-10-16 20:43:15 +0800

    20181016/0843

--- js/all.js
+++ js/all.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
     // masonry option
-    var $container = $('#container');
+    
+    var $container = $('.grid');
     $container.imagesLoaded(function(){
         $container.masonry({
-            itemSelector : '.item',
-            columnWidth : 30 
+            itemSelector : '.grid-item',
+            columnWidth : 30,
+            percentPosition: true,
+            gutter: 5 
         })
     })
-    var $life = $('#lifemoment');
+    var $life = $('.photo-wrap');
     $life.imagesLoaded(function(){
         $life.masonry({
             itemSelector : '.item',
-            columnWidth : 30 
+            columnWidth : 30,
+            percentPosition: true,
+            gutter: 5  
         })
     })
     
@@ -25,24 +30,31 @@ $(document).ready(function(){
     });
 
     //type effetc
-    $('#title').t({
+    $('.titleh1').t({
         // typing speed in milliseconds
         speed: 75,
         speed_vary: true
     });
-    $('#japan').t({
+    $('.japan').t({
         // typing speed in milliseconds
         speed: 75,
         speed_vary: true
     })
-    $('#life').t({
+    $('.life').t({
         // typing speed in milliseconds
         speed: 75,
         speed_vary: true
     })
     
     $('.fb').addClass('fab fa-facebook-square');
+    
+    $('.git').addClass('fab fa-github-square');
 })
 
-
+let app = new Vue({
+    el:'#footer',
+    data:{
+        message:' 2018 Ming-Yang Shih'
+    }
+});
 
